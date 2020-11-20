ee3451cdc474e9869cb754598b77aebd3ee620d1
Anxo Rodriguez <anxolin@gmail.com>
2020-11-20 18:41:14 +0100

    Get DutchX balances

--- src/commands/dxBalances.ts
+++ src/commands/dxBalances.ts
@@ -1,18 +1,63 @@
 import { CommanderStatic } from 'commander'
 import { readAddressesFromFile } from 'util/file'
+import { dxAddress, dxTokens } from 'const'
+import { Dutchx__factory } from 'contracts/gen/factories/Dutchx__factory'
+import { getProvider } from 'util/ethers'
+import chalk from 'chalk'
+import { BigNumber } from 'ethers'
+import { Dutchx } from 'contracts/gen'
+
+interface Balances {
+  user: string
+  balances: Map<string, BigNumber>
+}
+
+async function getBalances(user: string, dxContract: Dutchx): Promise<Balances> {
+  const balancePromises = dxTokens.map(async (token) => {
+    return {
+      token,
+      balance: await dxContract.balances(token, user),
+    }
+  })
+  const balances = await Promise.all(balancePromises)
+
+  return {
+    user,
+    balances: balances.reduce((acc, balanceItem) => {
+      const { token, balance } = balanceItem
+
+      // Return all non-zero balances
+      if (!balance.isZero) {
+        acc.set(token, balance)
+      }
+
+      return acc
+    }, new Map()),
+  }
+}
 
 async function run(file: string): Promise<void> {
-  const result = readAddressesFromFile(file)
-  if (result.isError) {
-    console.error(result.errorMsg)
+  const users = readAddressesFromFile(file)
+  if (users.isError) {
+    console.error(users.errorMsg)
     return
   }
 
-  // const provider = getProvider()
-  // const balance = await provider.getBalance(address)
+  const provider = getProvider()
+  const dxContract = Dutchx__factory.connect(dxAddress, provider)
+
+  const balancePromises = users.value.map((user) => getBalances(user, dxContract))
+  const balances = (await Promise.all(balancePromises)).filter(({ balances }) => balances.size > 0)
+
+  balances.forEach(({ user, balances }) => {
+    console.log(chalk`User ${user}:`)
+    for (const [token, balance] of balances) {
+      console.log(`token ${token}, balance = ${balance}`)
+    }
+  })
+  console.log(chalk`TOTAL {white ${balances.length}} users with balance in DutchX`)
 
   // console.log(chalk`Balance: {green ${ethers.utils.formatEther(balance)} ETH} (${balance} wei)`)
-  console.log('result', result)
 }
 
 export function registerCommand(program: CommanderStatic): void {
---
+++ src/const.ts
@@ -0,0 +1,4 @@
+// DutchX address:
+//  https://dutchx.readthedocs.io/en/latest/smart-contracts_addresses.html
+export const dxAddress = '0xb9812e2fa995ec53b5b6df34d21f9304762c5497'
+export const dxTokens = ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2']
