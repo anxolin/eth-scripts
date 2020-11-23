f095dac453b49a6d863afc8a47f3147690b74dba
Anxo Rodriguez <anxolin@gmail.com>
2020-11-23 08:32:27 +0100

    Add with retry util

---
+++ src/util/misc.ts
@@ -0,0 +1,16 @@
+import chalk from 'chalk'
+
+const WAIT_TIME = 10000
+
+export async function withRetry<T>(run: () => Promise<T>, attempts = 100): Promise<T> {
+  return run().catch((error) => {
+    console.error(chalk`{red Error: ${error}. Retrying in {white ${WAIT_TIME}ms} }`)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        withRetry(run, attempts - 1)
+          .then(resolve)
+          .catch(reject)
+      }, WAIT_TIME)
+    })
+  })
+}
