a6d334bc3dc4905863cd8b9c35210973df09a45d
Anxo Rodriguez <anxolin@gmail.com>
2020-11-24 10:26:05 +0100

    Remove tokens from consts, now uses file

--- src/const.ts
+++ src/const.ts
@@ -1,36 +1,6 @@
+export const DEFAULT_DECIMALS = 18
+
 // DutchX address:
 //  https://dutchx.readthedocs.io/en/latest/smart-contracts_addresses.html
 export const dxAddress = '0xb9812e2fa995ec53b5b6df34d21f9304762c5497'
 export const dxDeploymentBlock = 7189412
-export const dxTokens = [
-  '0x6810e776880c02933d47db1b9fc05908e5386b96', // GNO
-  '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
-  '0xdd974d5c2e2928dea5f71b9825b8b646686bd200', // Kyber
-  '0x2c4e8f2d746113d0696ce89b35f0d8bf88e0aeca', // OST
-  '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359', // SAI
-  '0x744d70fdbe2ba4cf95131626614a1763df805b9e', // SNT - status
-  '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07', // OMG
-  '0x543ff227f64aa17ea132bf9886cab5db55dcaddf', // GEN
-  '0x255aa6df07540cb5d3d297f0d0d4d84cb52bc8e6', // RDN
-  '0x6c6ee5e31d828de241282b9606c8e98ea48526e2', // HOLO
-  '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MAKER
-  '0x0cf0ee63788a0849fe5297f3407f701e122cc023', // DATAcoin
-  // '0x93ed3fbe21207ec2e8f2d3c3de6e058cb73bc04d', // Pinakion
-  // '0xbbbbca6a901c926f240b89eacb641d8aec7aeafd', // Loopring
-  // '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-  // '0x12b19d3e2ccc14da04fae33e63652ce469b3f2fd', // GRID
-  // '0xc12d1c73ee7dc3615ba4e37e4abfdbddfa38907e', // Kick token
-  // '0x1776e1f26f98b1a5df9cd347953a26dd3cb46671', // Numeraire
-  // '0x514910771af9ca656af840dff83e8264ecf986ca', // Chainlink
-  // '0x0abdace70d3790235af448c88547603b945604ea', // 0x
-  // '0x8f8221afbb33998d8584a2b05749ba73c37a938a', // REQ
-  // '0x0f5d2fb29fb7d3cfee444a200298f468908cc942', // Decentraland
-  // '0x41e5560054824ea6b0732e656e3ad64e20e94e45', // Civic
-  // '0x1f573d6fb3f13d689ff844b4ce37794d79a7ff1c', // Bancor
-  // '0x419d0d8bdd9af5e606ae2232ed285aff190e711b', // Funfair
-  // '0xe41d2489571d322189246dafa5ebde1f4699f498', // ZRX
-  // '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
-  // '0x5732046a883704404f284ce41ffadd5b007fd668', // Buzelle
-  // '0x0d8775f648430679a709e98d2b0cb6250d2887ef', // BAT
-  // '0xa4e8c3ec456107ea67d3075bf9e3df3a75823db0', // LOOM
-]
