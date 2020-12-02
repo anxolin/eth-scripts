b39fba625a82431cfa23f49bcb6d436fc647a06d
Anxo Rodriguez <anxolin@gmail.com>
2020-12-02 15:17:09 +0100

    Create utlis and simplify command

--- src/commands/uniswap/sellPrice.ts
+++ src/commands/uniswap/sellPrice.ts
@@ -1,52 +1,45 @@
-import { ChainId, Fetcher, Pair, Route, TokenAmount, Trade, TradeType } from '@uniswap/sdk'
+import { ChainId, Pair, Route, TokenAmount, Trade, TradeType } from '@uniswap/sdk'
 import chalk from 'chalk'
 import { CommanderStatic } from 'commander'
 import { ethers } from 'ethers'
 import { getProvider } from 'util/ethers'
-import { getTokensDetails } from 'util/tokens'
-import { printPrice, toToken } from './uniswapUtils'
+import { getPairInfo, printPrice } from './uniswapUtils'
 
 async function run(sellTokenAddress: string, receiveTokenAddress: string, amount?: string) {
   const chainId = ChainId.MAINNET
   const provider = getProvider()
-  const [sellTokenDetails, receiveTokenDetails] = await getTokensDetails(
-    [sellTokenAddress, receiveTokenAddress],
+  const { pair, receiveTokenLabel, sellTokenLabel } = await getPairInfo(
+    sellTokenAddress,
+    receiveTokenAddress,
+    chainId,
     provider,
   )
+  const pairAddress = Pair.getAddress(pair.token0, pair.token1)
 
-  const sellToken = toToken(chainId, sellTokenDetails)
-  const receiveToken = toToken(chainId, receiveTokenDetails)
-  const pairAddress = Pair.getAddress(sellToken, receiveToken)
-  const pair = await Fetcher.fetchPairData(sellToken, receiveToken, provider)
-
-  const route = new Route([pair], sellToken)
+  const route = new Route([pair], pair.token0)
 
   console.log()
   console.log(chalk`{bold Trade}:`)
+  console.log(chalk`\tSell Token: {yellow ${sellTokenLabel}} (${pair.token0.decimals}): {white ${pair.token0.address}}`)
   console.log(
-    chalk`\tSell Token: {yellow ${sellTokenDetails.label}} (${sellTokenDetails.decimals}): {white ${sellTokenDetails.address}}`,
-  )
-  console.log(
-    chalk`\tReceive Token: {yellow ${receiveTokenDetails.label}} (${receiveTokenDetails.decimals}): {white ${receiveTokenDetails.address}}`,
+    chalk`\tReceive Token: {yellow ${receiveTokenLabel}} (${pair.token1.decimals}): {white ${pair.token1.address}}`,
   )
   console.log(chalk`\tSell Amount: {yellow ${amount}}`)
 
   console.log()
   console.log(chalk`{bold Pools}:`)
   console.log(chalk`\tPair Address: {white ${pairAddress}}`)
-  console.log(chalk`\t${sellTokenDetails.label} Reserve: {white ${pair.reserve0.toExact()} ${sellTokenDetails.label}}`)
-  console.log(
-    chalk`\t${receiveTokenDetails.label} Reserve: {white ${pair.reserve1.toExact()} ${receiveTokenDetails.label}}`,
-  )
+  console.log(chalk`\t${sellTokenLabel} Reserve: {white ${pair.reserve0.toExact()} ${sellTokenLabel}}`)
+  console.log(chalk`\t${receiveTokenLabel} Reserve: {white ${pair.reserve1.toExact()} ${receiveTokenLabel}}`)
 
   console.log()
   console.log(chalk`{bold Prices}:`)
-  printPrice('\tMid Price', route.midPrice, sellTokenDetails, receiveTokenDetails)
+  printPrice('\tMid Price', route.midPrice, sellTokenLabel, receiveTokenLabel)
   if (amount) {
-    const amountInWei = ethers.utils.parseUnits(amount, sellToken.decimals)
-    const trade = new Trade(route, new TokenAmount(sellToken, amountInWei.toString()), TradeType.EXACT_INPUT)
-    printPrice('\tExecution Price', trade.executionPrice, sellTokenDetails, receiveTokenDetails)
-    printPrice('\tNext Mid Price', trade.nextMidPrice, sellTokenDetails, receiveTokenDetails)
+    const amountInWei = ethers.utils.parseUnits(amount, pair.token0.decimals)
+    const trade = new Trade(route, new TokenAmount(pair.token0, amountInWei.toString()), TradeType.EXACT_INPUT)
+    printPrice('\tExecution Price', trade.executionPrice, sellTokenLabel, receiveTokenLabel)
+    printPrice('\tNext Mid Price', trade.nextMidPrice, sellTokenLabel, receiveTokenLabel)
   }
 }
 
--- src/commands/uniswap/uniswapUtils.ts
+++ src/commands/uniswap/uniswapUtils.ts
@@ -1,16 +1,49 @@
-import { ChainId, Price, Token } from '@uniswap/sdk'
+import { ChainId, Fetcher, Pair, Price, Token } from '@uniswap/sdk'
 import chalk from 'chalk'
 import { DEFAULT_PRICE_DECIMALS } from 'const'
+import { providers } from 'ethers'
 import { TokenDetails } from 'types'
+import { getTokensDetails } from 'util/tokens'
+
+interface PairInfo {
+  pair: Pair
+  // Just for convenience, the Pair we also return the labels
+  sellTokenLabel: string
+  receiveTokenLabel: string
+}
+
+export async function getPairInfo(
+  sellTokenAddress: string,
+  receiveTokenAddress: string,
+  chainId: ChainId,
+  provider: providers.BaseProvider,
+): Promise<PairInfo> {
+  const [sellTokenDetails, receiveTokenDetails] = await getTokensDetails(
+    [sellTokenAddress, receiveTokenAddress],
+    provider,
+  )
+
+  const sellToken = toToken(chainId, sellTokenDetails)
+  const receiveToken = toToken(chainId, receiveTokenDetails)
+  const pair = await Fetcher.fetchPairData(sellToken, receiveToken, provider)
+
+  return {
+    pair,
+    sellTokenLabel: sellTokenDetails.label,
+    receiveTokenLabel: receiveTokenDetails.label,
+  }
+}
 
 export function toToken(chainId: ChainId, tokenDetails: TokenDetails): Token {
   return new Token(chainId, tokenDetails.address, tokenDetails.decimals, tokenDetails.symbol, tokenDetails.name)
 }
 
-export function printPrice(label: string, price: Price, sellToken: TokenDetails, receiveToken: TokenDetails): void {
+export function printPrice(label: string, price: Price, sellTokenLabel: string, receiveTokenLabel: string): void {
   console.log(
-    chalk`${label}: {white ${price.toSignificant(DEFAULT_PRICE_DECIMALS)}} {yellow ${sellToken.label} per ${
-      receiveToken.label
-    }} (${price.invert().toSignificant(DEFAULT_PRICE_DECIMALS)} ${receiveToken.label} per ${sellToken.label})`,
+    chalk`${label}: {white ${price.toSignificant(
+      DEFAULT_PRICE_DECIMALS,
+    )}} {yellow ${sellTokenLabel} per ${receiveTokenLabel}} (${price
+      .invert()
+      .toSignificant(DEFAULT_PRICE_DECIMALS)} ${receiveTokenLabel} per ${sellTokenLabel})`,
   )
 }
