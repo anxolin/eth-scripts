cad9fdcb087b5c5b1a019231b1d34617c898fd44
Anxo Rodriguez <anxolin@gmail.com>
2020-12-03 10:54:21 +0100

    Add get signer method

--- src/util/ethers.ts
+++ src/util/ethers.ts
@@ -1,10 +1,12 @@
-import { ethers, providers } from 'ethers'
+import { ethers, providers, Signer } from 'ethers'
 import Debug from 'debug'
 import assert from 'assert'
 
 const debug = Debug('DEBUG-app:util:ethers')
 
 let provider: providers.BaseProvider
+let signer: Signer
+
 export function getProvider(): providers.BaseProvider {
   if (!provider) {
     const nodeUrl = process.env.NODE_URL
@@ -14,4 +16,15 @@ export function getProvider(): providers.BaseProvider {
   }
 
   return provider
+}
+
+export function getSigner(): Signer {
+  if (!signer) {
+    if (!process.env.PK) {
+      throw new Error('Private Key is required. Define a "PK" environment var')
+    }
+    signer = new ethers.Wallet('0x' + process.env.PK, getProvider())
+  }
+
+  return signer
 }
