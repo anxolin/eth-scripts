30e2491888742327504ce5b5f1561f3a19192cbb
Anxo Rodriguez <anxolin@gmail.com>
2021-06-01 17:19:05 +0200

    Improve get user deposits

--- src/commands/dutchX/usersDeposit.ts
+++ src/commands/dutchX/usersDeposit.ts
@@ -1,30 +1,20 @@
 import chalk from 'chalk'
 import { CommanderStatic } from 'commander'
-import { dxAddress, dxDeploymentBlock } from 'const'
-import { Dutchx__factory } from 'contracts/gen/factories/Dutchx__factory'
-import { getProvider } from 'util/ethers'
 import { writeJson } from 'util/file'
-
-const BATCH_SIZE = 100000 // num of blocks
+import { getDutchX, getEventData } from './utils'
 
 async function run(filePath: string): Promise<void> {
-  const provider = getProvider()
-  const dxContract = Dutchx__factory.connect(dxAddress, provider)
-  const allDepositFilter = dxContract.filters.NewDeposit(null, null)
-  const blockNumber = await provider.getBlockNumber()
-  const users: Set<string> = new Set()
-  for (let fromBlock = dxDeploymentBlock; fromBlock < blockNumber; fromBlock += BATCH_SIZE) {
-    const toBlock = fromBlock + BATCH_SIZE - 1
-    console.log(chalk`Query deposits from block {yellow ${fromBlock}} and {yellow ${toBlock}}`)
-    const allDeposits = await dxContract.queryFilter(allDepositFilter, fromBlock, toBlock)
-    const addressesPromise = allDeposits.map(async (event) =>
-      event.getTransactionReceipt().then((receipt) => receipt.from),
-    )
-    const addresses = await Promise.all(addressesPromise)
-    addresses.forEach((user) => users.add(user))
-    console.log(chalk`\tFound {white ${addresses.length}} deposits. Total users {white ${users.size}}\n`)
-  }
+  const dxContract = getDutchX()
 
+  const usersWithDeposit = await getEventData({
+    itemsLabel: 'deposits',
+    eventFilter: dxContract.filters.NewDeposit(null, null),
+    getDataFromEvent: async (event) =>
+      // Note, we cannot get the original message sender. For getting users, is best to
+      event.getTransactionReceipt().then((receipt) => receipt.from),
+  })
+
+  const users = new Set(usersWithDeposit)
   console.log(chalk`TOTAL {white ${users.size}} users deposited tokens`)
   writeJson(filePath, Array.from(users))
   console.log(chalk`Written users in file {white ${filePath}}`)
